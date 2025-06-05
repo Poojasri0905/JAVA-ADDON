04497a7dde379f78de07a107c8bcdb7ed4475886
Poojasri0905 <poojasrirajendran09@gmail.com>
2025-06-05 21:30:19 +0530

    Update and rename Main.java to Voting.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class Main{
-public static void main(String[] args) {
-Stsyem.out.println("Enter your age");
-Scanner ref=new Scanner(System.in);
-int a=ref.nextInt();
-System.out.println(a);
-}
-}
