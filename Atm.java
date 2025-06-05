d1bdb376640d411e56bfeff54e00470af3cc54ac
Poojasri0905 <poojasrirajendran09@gmail.com>
2025-06-05 21:27:12 +0530

    Update Atm.java

--- Atm.java
+++ Atm.java
@@ -1,21 +1,26 @@
-import java.util.Scanner;
-public class Atm {
-  public static void main(String[] args) {
-    Scanner ref = new Scanner(System.in);
-    System.out.print("Enter your PIN: ");
-    int pin=ref.nextInt();
-    System.out.println("Enter your amount");
-    int amount=ref.nextInt();
-    if(pin == 1234) {
-             if(amount <= 50000){
-                      //withdraw successfully
-              }
-              else{
-              //insufficient  
-              }
+import java.util.*;
+class Atm{
+    public static void main(String[] args){
+        Scanner ref=new Scanner(System.in);
+        System.out.println("enter pin");
+        int pin=ref.nextInt();
+        if(pin==1234){
+            System.out.println("enter amount");
+        int amount=ref.nextInt();
+             if(amount<= 5000)    {
+        
+        System.out.println("withdrawn successfully");
+        }
+        else{
+            System.out.println("insufficient  amount");
+            
+        }
+        
+    } 
+        else{
+            System.out.println("invalid ");
+        }
+    
+    }
 }
-else{
-//invalid
-}
-}
-}
+    
