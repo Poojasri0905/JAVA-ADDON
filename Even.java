56144f6ee72a1da7d99294d41a4bdc0f28371894
Poojasri0905 <poojasrirajendran09@gmail.com>
2025-06-05 12:36:24 +0530

    Create Even.java

---
+++ Even.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Even {
+    public static void main(String[] args) {
+        System.out.print("Enter the starting value:");
+         Scanner in=new Scanner(System.in);
+        int a=in.nextInt();
+System.out.println(Enter the ending value:");
+        int b=in.nextInt();
+        for(int i=a; i<b; i++){
+        if (i % 2 == 0) {
+      System.out.println("i");
+ }
+}
+}
+}
+  
+
