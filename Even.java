f3a743a6709cdfccbc81c42b1771ada50aadd440
Poojasri0905 <poojasrirajendran09@gmail.com>
2025-06-05 21:28:56 +0530

    Update Even.java

--- Even.java
+++ Even.java
@@ -1,18 +1,15 @@
-import java.util.Scanner;
-
-public class Even {
+import java.util.*;
+    public class Even {
     public static void main(String[] args) {
-        System.out.print("Enter the starting value:");
-         Scanner in=new Scanner(System.in);
+        System.out.println("Enter the starting value:");
+        Scanner in=new Scanner(System.in);
         int a=in.nextInt();
-System.out.println(Enter the ending value:");
+        System.out.println("Enter the ending value:");
         int b=in.nextInt();
-        for(int i=a; i<b; i++){
-        if (i % 2 == 0) {
-      System.out.println("i");
- }
+        for (int i =a; i<=b; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
 }
-}
-}
-  
-
