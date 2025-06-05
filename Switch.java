5d40b7c71a88729ead67e97de0cf8a284f08aebd
Poojasri0905 <poojasrirajendran09@gmail.com>
2025-06-05 15:14:03 +0530

    Create Switch.java

---
+++ Switch.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Switch {
+    public static void main(String[] args) {
+        int operator
+        switch(a=b);
+                case 1;
+                    System.out.println("a+b:");
+                    break;
+                case 2:
+                    System.out.println("a-b:");
+                    break;
+                case 3:
+                    System.out.println("a*b:");
+                    break;
+                case 4:
+                    System.out.println("a/b:");
+                    break;
+                
+    }
+    
+}
+    }}Switch
