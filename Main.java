c1191fd779a2116867c7c88043b7f7a798330679
Poojasri0905 <poojasrirajendran09@gmail.com>
2025-06-05 11:42:27 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Main{
+public static void main(String[] args) {
+Stsyem.out.println("Enter your age");
+Scanner ref=new Scanner(System.in);
+int a=ref.nextInt();
+System.out.println(a);
+}
+}
